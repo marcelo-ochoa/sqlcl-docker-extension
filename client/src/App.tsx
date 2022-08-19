8b6b6fe36e0b1625b8a3d7219f6d0101c5296f56
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2022-08-19 12:28:18 +0200

    Adapt theme depending on the system

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
-import { useState, useEffect } from 'react';
-import { LinearProgress, Typography, Grid } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Box, Grid, LinearProgress, Typography, useMediaQuery } from '@mui/material';
 import { createDockerDesktopClient } from '@docker/extension-api-client';
-import { useMediaQuery} from '@mui/material';
 
 
 const client = createDockerDesktopClient();
@@ -11,25 +10,41 @@ function useDockerDesktopClient() {
 }
 
 export function App() {
-  const [ready, setReady] = useState<boolean>(false);
+  const [started, setStarted] = useState(false);
+  const [ready, setReady] = useState(false);
   const ddClient = useDockerDesktopClient();
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
-  if (prefersDarkMode) {
-    ddClient.extension.vm?.service?.get('/dark');
-  } else {
-    ddClient.extension.vm?.service?.get('/light');
-  }
+  useEffect(() => {
+    const start = async (darkMode: boolean) => {
+      setStarted(() => false);
+      setReady(() => false);
+
+      if (darkMode) {
+        await ddClient.extension.vm?.service?.post('/dark', null);
+      } else {
+        await ddClient.extension.vm?.service?.post('/light', null);
+      }
+
+      setStarted(() => true);
+    };
+
+    start(prefersDarkMode);
+  }, [prefersDarkMode]);
 
   useEffect(() => {
-    const checkIfsqlclIsReady = async () => {
+    if (!started || ready) {
+      return;
+    }
 
+
+    const checkIfsqlclIsReady = async () => {
       const result = await ddClient.extension.vm?.service?.get('/ready');
       const ready = Boolean(result);
       if (ready) {
         clearInterval(timer);
       }
-      setReady(ready);
+      setReady(() => ready);
     };
 
     let timer = setInterval(() => {
@@ -39,27 +54,26 @@ export function App() {
     return () => {
       clearInterval(timer);
     };
-  }, []);
-
-  useEffect(() => {
-    if (ready) {
-      window.location.href = 'http://localhost:9890/';
-    }
-  }, [ready]);
+  }, [started, ready]);
 
   return (
-    <Grid container flex={1} direction="column" spacing={4}>
-      <Grid item justifyContent="center" textAlign="center" minHeight="80px">
-        {!ready && (
-          <>
-            <LinearProgress />
+    <>
+      {!ready && (
+        <Grid container flex={1} direction="column" padding="16px 32px" height="100%" justifyContent="center" alignItems="center">
+          <Grid item>
+            <LinearProgress/>
             <Typography mt={2}>
               Waiting for sqlcl to be ready. It may take some seconds if
               it's the first time.
             </Typography>
-          </>
-        )}
-      </Grid>
-    </Grid>
+          </Grid>
+        </Grid>
+      )}
+      {ready && (
+        <Box display="flex" flex={1} width="100%" height="100%">
+          <iframe src='http://localhost:9890/' width="100%" height="100%" />
+        </Box>
+      )}
+    </>
   );
 }
--- vm/main.go
+++ vm/main.go
@@ -12,7 +12,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var daemonStarted = false
+var ttyd TTYD
 
 func main() {
 	var socketPath string
@@ -34,9 +34,11 @@ func main() {
 	}
 	router.Listener = ln
 
+	ttyd = TTYD{}
+
 	router.GET("/ready", ready)
-	router.GET("/dark", dark)
-	router.GET("/light", light)
+	router.POST("/dark", dark)
+	router.POST("/light", light)
 
 	log.Fatal(router.Start(startURL))
 }
@@ -47,44 +49,107 @@ func listen(path string) (net.Listener, error) {
 
 // ready checks whether sqlcl is ready or not by querying localhost:9080.
 func ready(ctx echo.Context) error {
-	url := "http://localhost:7681/" // "sqlcl" is the name of the service defined in docker-compose.yml
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Println(err)
-		return ctx.String(http.StatusOK, "false")
-
+	if ttyd.IsRunning() {
+		return ctx.String(http.StatusOK, "true")
 	}
-	defer resp.Body.Close()
 
-	return ctx.String(resp.StatusCode, "true")
-
-	// return ctx.JSON(http.StatusOK, HTTPMessageBody{Message: "hello from HTTP"})
-
+	return ctx.String(http.StatusServiceUnavailable, "false")
 }
 
 // dark starts ttyd in dark mode
 func dark(ctx echo.Context) error {
-	if !daemonStarted {
-		cmd := exec.Command("/usr/bin/ttyd", "-u", "1000", "-g", "1000", "-t", "titleFixed='sqlcl'", "/bin/bash", "/home/sql.sh")
-		cmd.Start()
-		daemonStarted = true
-	}
-	return ctx.String(http.StatusOK, "true")
+	if err := ttyd.Start(DarkTheme); err != nil {
+		log.Printf("failed to start ttyd with dark mode: %s\n", err)
 
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	log.Println("started ttyd with dark mode")
+
+	return ctx.String(http.StatusOK, "true")
 }
 
 // light starts ttyd in dark mode
 func light(ctx echo.Context) error {
-	if !daemonStarted {
-		cmd := exec.Command("/usr/bin/ttyd", "-u", "1000", "-g", "1000", "-t", "titleFixed='sqlcl'", "-t", "theme={'background': '#ffffff', 'foreground': '#2b2b2b', 'cursor': '#adadad', 'selection': '#ddb6fc'}", "/bin/bash", "/home/sql.sh")
-		cmd.Start()
-		daemonStarted = true
-	}
-	return ctx.String(http.StatusOK, "true")
+	if err := ttyd.Start(LightTheme); err != nil {
+		log.Printf("failed to start ttyd with light mode: %s\n", err)
 
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	log.Println("started ttyd with light mode")
+
+	return ctx.String(http.StatusOK, "true")
 }
 
 type HTTPMessageBody struct {
 	Message string `json:"message"`
 	Body    string `json:"body,omitempty"`
+}
+
+type Theme string
+
+const (
+	DarkTheme  Theme = "dark"
+	LightTheme Theme = "light"
+)
+
+type TTYD struct {
+	process *os.Process
+}
+
+func (t *TTYD) Start(theme Theme) error {
+	if t.IsStarted() {
+		if err := t.Stop(); err != nil {
+			log.Printf("failed to stop ttyd: %s\n", err)
+		}
+	}
+
+	args := []string{"-u", "1000", "-g", "1000", "-t", "titleFixed='sqlcl'"}
+	if theme == LightTheme {
+		args = append(args, "-t", "theme={'background': '#ffffff', 'foreground': '#2b2b2b', 'cursor': '#adadad', 'selection': '#ddb6fc'}")
+	}
+
+	args = append(args, "/bin/bash", "/home/sql.sh")
+
+	cmd := exec.Command("/usr/bin/ttyd", args...)
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	t.process = cmd.Process
+
+	return nil
+}
+
+func (t *TTYD) Stop() error {
+	if !t.IsStarted() {
+		return nil
+	}
+
+	if err := t.process.Kill(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t TTYD) IsStarted() bool {
+	return t.process != nil
+}
+
+func (t *TTYD) IsRunning() bool {
+	if !t.IsStarted() {
+		return false
+	}
+
+	url := "http://localhost:7681/" // "sqlcl" is the name of the service defined in docker-compose.yml
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+		return false
+
+	}
+
+	return resp.StatusCode == http.StatusOK
 }
